21e49ab1db7dc411b35022fa718711f56fcea288
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-02-07 08:05:47 +0530

    Day-1 07-02-2023

---
+++ Welcome.java
@@ -0,0 +1,17 @@
+class Welcome
+{
+public static void main(String args[])
+{
+System.out.println("Leave Us its already 7:45");
+}
+
+//machine language-->binary 0's 1's
+}//shiva -->010101010101     a->97-->000110101
+
+
+
+//  .java user understandable --->  machine language--  .class
+
+//jdk-->jre-->jvm		compiler	compailation
+
+//javac filename.java
